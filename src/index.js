a3801a874c9fa3abe6ac63cba3d3c5e3a51334c5
its-pablo <78528552+stretch07@users.noreply.github.com>
2023-04-26 01:01:14 +0000

    idek anymore

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 import MsgroomBot from "msgroom-bot"
+import { BingChat } from "bing-chat"
 const bot = new MsgroomBot
 import {ChatGPTAPI} from "chatgpt"
 
-const api = new ChatGPTAPI({
+const chatApi = new ChatGPTAPI({
   apiKey: process.env.OPENAI_API_KEY
+})
+const bingApi = new BingChat({
+  cookie: process.env.BING_COOKIE
 })
 
 bot.connect("chatgpt")
 const cmse = bot.registerCommandSet("gpt!")
 cmse.registerCommand("send", async (...args) => {
   bot.send("One moment...")
-  const res = await api.sendMessage(args.join(" "))
+  const res = await chatApi.sendMessage(args.join(" "))
+  bot.send(res.text)
+})
+cmse.registerCommand("bing", async (...args) => {
+  bot.send("One moment...")
+  const res = await bingApi.sendMessage(args.join(" "))
   bot.send(res.text)
 })
