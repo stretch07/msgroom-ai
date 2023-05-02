d49ac17d1670d733bd9427dfbae3cbdeb6da42a0
its-pablo <78528552+its-pablo@users.noreply.github.com>
2023-05-02 22:47:23 +0000

    Buggy Google Bard support

--- src/index.js
+++ src/index.js
@@ -1,24 +1,43 @@
 import MsgroomBot from "msgroom-bot"
 import { BingChat } from "bing-chat"
+import { ChatGPTAPI } from "chatgpt"
+import { Bard } from "googlebard"
 const bot = new MsgroomBot
-import {ChatGPTAPI} from "chatgpt"
 
-const chatApi = new ChatGPTAPI({
-  apiKey: process.env.OPENAI_API_KEY
-})
-const bingApi = new BingChat({
-  cookie: process.env.BING_COOKIE
-})
+let chatApi
+if (process.env.OPENAI_API_KEY) {
+  chatApi = new ChatGPTAPI({
+    apiKey: process.env.OPENAI_API_KEY
+  })
+}
+let bingApi
+if (process.env.BING_COOKIE) {
+  bingApi = new BingChat({
+    cookie: process.env.BING_COOKIE
+  })
+}
+let bardApi
+if (process.env.BARD_COOKIE) {
+  bardApi = new Bard(process.env.BARD_COOKIE)
+}
 
-bot.connect("chatgpt")
-const cmse = bot.registerCommandSet("gpt ")
+bot.connect("ai")
+const cmse = bot.registerCommandSet("ai ") //space is required yes
 cmse.registerCommand("openai", async (...args) => {
-  bot.send("One moment...")
+  bot.changeNick("ai [thinking]")
   const res = await chatApi.sendMessage(args.join(" "))
+  bot.changeNick("ai")
   bot.send(res.text)
 })
 cmse.registerCommand("bing", async (...args) => {
-  bot.send("One moment...")
+  bot.changeNick("ai [thinking]")
   const res = await bingApi.sendMessage(args.join(" "))
+  bot.changeNick("ai")
+  bot.send(res.text)
+})
+cmse.registerCommand("bard", async (...args) => {
+  bot.changeNick("ai [thinking]")
+  const res = await bardApi.ask(args.join(" "))
+  bot.changeNick("ai")
   bot.send(res.text)
 })
